7f85bea606e71344e376ab7519be3082fec0ee7c
Michal Mrena <mrena.michal@gmail.com>
2023-09-25 12:27:34 +0200

    Task 4.5

--- Arena.java
+++ Arena.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class Arena here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class Arena extends World
 {
 
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public Arena()
     {    
         // Create a new world with 25x15 cells with a cell size of 60x60 pixels.
         super(25, 15, 60); 
