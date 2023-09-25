9477fb9718f0269d4025491d9160ad37da8636b7
Michal Mrena <mrena.michal@gmail.com>
2023-09-25 13:42:50 +0200

    Task 6.3

--- Arena.java
+++ Arena.java
@@ -8,23 +8,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Arena extends World
 {
-
-    private Player player1;
-    private Player player2;
-    
     /**
-     * Constructor for objects of class MyWorld.
-     * 
+     * Constructor for objects of class Arena. 
      */
-    public Arena()
+    public Arena(int width, int height)
     {    
-        // Create a new world with 25x15 cells with a cell size of 60x60 pixels.
-        super(25, 15, 60);
-
-        this.player1 = new Player("up", "down", "right", "left", "space");
-        this.addObject(this.player1, 0, 0);
-        
-        this.player2 = new Player("w", "s", "d", "a", "x");
-        this.addObject(this.player2, 24, 14);
+        // Create a new world with width x height cells with a cell size of 60x60 pixels.
+        super(width, height, 60);
     }
 }
