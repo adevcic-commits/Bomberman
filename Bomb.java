45fe4733e5ffeb58fee0fe2bc9e1d4af8322b0d4
Michal Mrena <mrena.michal@gmail.com>
2023-09-25 13:15:41 +0200

    Task 5.4

---
+++ Bomb.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bomb here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bomb extends Actor
+{
+    private int power;
+    
+    public Bomb(int power) {
+        this.power = power;
+    }
+    
+    /**
+     * Act - do whatever the Bomb wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
