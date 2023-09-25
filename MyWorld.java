86da438686edad7900e8e11f595917f49209d346
Michal Mrena <mrena.michal@gmail.com>
2023-09-25 10:58:25 +0200

    Task 1.5

--- MyWorld.java
+++ MyWorld.java
@@ -15,7 +15,7 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 25x15 cells with a cell size of 60x60 pixels.
+        super(25, 15, 60); 
     }
 }
