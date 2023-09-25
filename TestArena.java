ba2f48e3a8f38503342d26e9dbae6e3a68f1e2ae
Michal Mrena <mrena.michal@gmail.com>
2023-09-25 13:39:10 +0200

    Code 6.1

---
+++ TestArena.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TestArena here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TestArena extends Arena
+{
+
+    /**
+     * Constructor for objects of class TestArena.
+     * 
+     */
+    public TestArena()
+    {
+    }
+}
