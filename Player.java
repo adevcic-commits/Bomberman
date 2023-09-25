61415cb24c76a38280e3d82a9a5cf5104d72c5e9
Michal Mrena <mrena.michal@gmail.com>
2023-09-25 11:37:24 +0200

    Code 3.3

--- Player.java
+++ Player.java
@@ -16,36 +16,48 @@ public class Player extends Actor
     {
         if (Greenfoot.isKeyDown("m")) {
             this.move(1);
-            
+
             // if the player is at the top edge of the world
             if (this.getY() == 0) {
                 // set the rotation downwards
                 this.setRotation(90);
-            }
-
-            // if the player is at the right edge of the world
-            if (this.getX() == 24) {
-                // set the rotation to the left
-                this.setRotation(180);
-            }
-            
-            // if the player is at the bottom edge of the world
-            if (this.getY() == 14) {
-                // set the rotation upwards
-                this.setRotation(270);
-            }
-
-            // if the player is at the left edge of the world
-            if (this.getX() == 0) {
-                // set the rotation to the right
-                this.setRotation(0);
-            }
+            } 
+            // otherwise
+            else {
+                // if the player is at the right edge of the world
+                if (this.getX() == 24) {
+                    // set the rotation to the left
+                    this.setRotation(180);
+                }
+                // otherwise
+                else {
+                    // if the player is at the bottom edge of the world
+                    if (this.getY() == 14) {
+                        // set the rotation upwards
+                        this.setRotation(270);
+                    }
+                    //otherwise
+                    else {
+                        // if the player is at the left edge of the world
+                        if (this.getX() == 0) {
+                            // set the rotation to the right
+                            this.setRotation(0);
+                        } // left edge test
+                    } // bottom edge test
+                } // right edge test
+            } // upper edge test
             
             // if the player is touching a wall, he will turn 90° counterclockwise
             if (this.isTouching(Wall.class)) {
                 this.turn(-90);
             }
+
+            // if the player is touching a brick wall, he will turn 90° clockwise
+            if (this.isTouching(BrickWall.class)) {
+                this.turn(90);
+            }
         }
+
     }
     
     /**
