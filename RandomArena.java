f937ffbd36f64b0df7c9d904e974a2437d9c8959
Michal Mrena <mrena.michal@gmail.com>
2023-09-27 15:52:02 +0200

    Task 10.4

---
+++ RandomArena.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RandomArena here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RandomArena extends Arena
+{
+
+    /**
+     * Constructor for objects of class RandomArena.
+     * 
+     */
+    public RandomArena()
+    {
+        super(7, 7);
+        this.createRectangleOfWalls(1, 1, 3, 3, 1, 1);
+    }
+}
