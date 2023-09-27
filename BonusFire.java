df2b5f48f12f9cdc1de0528422dbda07e5051d59
Michal Mrena <mrena.michal@gmail.com>
2023-09-27 16:15:43 +0200

    Task 10.10

---
+++ BonusFire.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BonusFire here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BonusFire extends Bonus
+{
+    /**
+     * Act - do whatever the BonusFire wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
