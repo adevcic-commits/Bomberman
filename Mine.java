f3d63427a9a975f5205e483ea535c4440582de28
Michal Mrena <mrena.michal@gmail.com>
2023-09-27 14:53:12 +0200

    Task 9.10

--- Mine.java
+++ Mine.java
@@ -14,52 +14,35 @@ public class Mine extends Explosive
         super(owner);
         this.timer = 5;
     }
-    
-    /**
-     * Act - do whatever the Mine wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        boolean shouldExplode = false;
-        // the mine explodes when it touches fire
-        if (this.isTouching(Fire.class)) {
-            shouldExplode = true;
-        }
-        // the mine does not touch fire
-        else {
-            if (this.timer > 0) {
-                // if the timer has not expired yet
-                // the mine is not active
-                // so we just lower the timer
-                this.timer = this.timer - 1;
-            }
-            else {
-                // the timer has expired, the mine is active
-                // now we check whether some player stands on the mine
-                if (this.isTouching(Player.class)) {
-                    shouldExplode = true;
-                }
-            }
-        }
-
-        if (shouldExplode) {
-            // we let the player know that the mine exploded
-            if (super.owner != null) {
-                super.owner.mineExploded(this);
-            }
-            // create a fire in place of the mine
-            // before we remove it from the world
-            // (so that its coordinates are still available)
-            World world = this.getWorld();
-            world.addObject(new Fire(5), this.getX(), this.getY());
-            // remove the mine from the world
-            world.removeObject(this);
-        }
-    }
 
     public void printWhoYouAre() {
         World world = this.getWorld();
         world.showText("MINE", this.getX(), this.getY());
+    }
+
+    protected boolean shouldExplode() {
+        // the mine must explode if it touches a fire
+        if (this.isTouching(Fire.class)) {
+            return true;
+        }
+
+        if (this.timer > 0) {
+            // if the timer has not expired yet
+            // the mine is not active
+            // so we just lower the timer
+            this.timer = this.timer - 1;
+            // consequently, the mine cannot explode
+            return false;
+        }
+        else {
+            // the timer has expired, the mine is active
+            // it should explode if it touches a plauer
+            return this.isTouching(Player.class);
+        }
+    }
+
+    protected void explosion() {
+        World world = this.getWorld();
+        world.addObject(new Fire(5), this.getX(), this.getY());
     }
 }
